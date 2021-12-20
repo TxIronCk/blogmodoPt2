7cc7efa7eaf65b4fa07c015c9a4983f8604d9392
Cynthia Mixon <txironck@gmail.com>
2021-12-20 09:48:01 -0600

    "complete step one"

--- blogmodo/react-client/utils/markdownParser.js
+++ blogmodo/react-client/utils/markdownParser.js
@@ -1,5 +1,28 @@
 const markdownParser = function (text) {
-  // TODO - your code here!
+  if (contains(text, "**")) {
+    <i>{text}</i>;
+  }
+  if (contains(text, "_")) {
+    <b>{text}</b>;
+  }
+  if (contains(text, "******")) {
+    <h6>{text}</h6>;
+  }
+  if (contains(text, "*****")) {
+    <h5>{text}</h5>;
+  }
+  if (contains(text, "****")) {
+    <h4>{text}</h4>;
+  }
+  if (contains(text, "***")) {
+    <h3>{text}</h3>;
+  }
+  if (contains(text, "**")) {
+    <h2>{text}</h2>;
+  }
+  if (contains(text, "*")) {
+    <h1>{text}</h1>;
+  }
 };
 
 export default markdownParser;
