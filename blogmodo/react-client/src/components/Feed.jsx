650b5819e1c8241659f723d8bbd368a0f206b309
Cynthia Mixon <txironck@gmail.com>
2021-12-20 17:12:57 -0600

    Got the map function working in Feed.jsx

--- blogmodo/react-client/src/components/Feed.jsx
+++ blogmodo/react-client/src/components/Feed.jsx
@@ -5,35 +5,28 @@ console.log(samplePosts[0].author);
 //This should contain a 'map' function that will dynamically display the messages
 //I was able to use the samplePosts to display 'one' author...
 
-const Feed = (props) =>
-  function mapBloggers(props) {
-    const bloggers = props.samplePosts;
-    const blogs = bloggers.map((blog) => (
-      <BlogPost key={blog._id} value={blog} />
-    ));
-    return (
-      <div className="feed">
-        <ul>
-          <li className="feed-list-item">
-            <div className="feed-list-item-title" onClick={props.handleClick}>
-              {blog.title}
-            </div>
-            <div className="feed-list-item-byline">
-              <span className="feed-list-item-byline-author">
-                {blog.author}
-              </span>
-              3 days ago
-            </div>
-            <img
-              src="http://www.placecorgi.com/350/197"
-              onClick={props.handleClick}
-              className="feed-list-item-image"
-            />
-            <span className="feed-list-item-lede">{blog.body}</span>
-          </li>
-        </ul>
-      </div>
-    );
-  };
+const Feed = (props) => (
+  <div className="feed">
+    <ul>
+      {samplePosts.map((blog) => (
+        <li key={blog._id} className="feed-list-item">
+          <div className="feed-list-item-title" onClick={props.handleClick}>
+            {blog.title}
+          </div>
+          <div className="feed-list-item-byline">
+            <span className="feed-list-item-byline-author">{blog.author}</span>3
+            days ago
+          </div>
+          <img
+            src={blog.imageUrl}
+            onClick={props.handleClick}
+            className="feed-list-item-image"
+          />
+          <span className="feed-list-item-lede">{blog.body}</span>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default Feed;
